918ea0021d9e5b529a191028bc029dde041af3fe
sachetwasti1997 <sachetwasti61@gmail.com>
2024-07-28 06:06:40 +0530

    first commit

---
+++ src/test/java/com/microservices/config/server/config_server_application/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.config.server.config_server_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
